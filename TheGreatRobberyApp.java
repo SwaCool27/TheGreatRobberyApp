62786cddeb9e86ce80628c24dd0a22f70ee026c5
SwaCool27 <109013008+SwaCool27@users.noreply.github.com>
2023-07-26 21:17:42 +0530

    Add files via upload

---
+++ TheGreatRobberyApp.java
@@ -0,0 +1,15 @@
+package Intermediate.greatRobbery;
+
+public class TheGreatRobberyApp {
+
+    public static void main(String[] args) {
+        City city = new City();
+        Gang theGang = new Gang();
+        Police police = new Police();
+        theGang.printGangInfo();
+        do {
+            theGang.letsRob(city.getBuildings());
+        } while (!police.catchCriminals(theGang));
+    }
+}
+
